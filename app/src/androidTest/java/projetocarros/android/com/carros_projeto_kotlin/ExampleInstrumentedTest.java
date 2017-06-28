fcac39c9679094e1f82be19e86e75c3cdcc93b91
Renato Lopes <renatollopezzdev@gmail.com>
2017-06-27 20:39:33 -0400

    Initial commit

---
+++ app/src/androidTest/java/projetocarros/android/com/carros_projeto_kotlin/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package projetocarros.android.com.carros_projeto_kotlin;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("projetocarros.android.com.carros_projeto_kotlin", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/projetocarros/android/com/carros_projeto_kotlin/MainActivity.java
@@ -0,0 +1,13 @@
+package projetocarros.android.com.carros_projeto_kotlin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
